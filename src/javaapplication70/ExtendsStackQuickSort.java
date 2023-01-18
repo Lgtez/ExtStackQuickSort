c4c4dc0b443b4749c99e3dbcabbc62815a407632
lgtez <lgtez@DESKTOP-LG5PVA5>
2023-01-18 14:33:20 -0600

    Ver 1.0

---
+++ src/javaapplication70/ExtendsStackQuickSort.java
@@ -0,0 +1,20 @@
+
+package javaapplication70;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class ExtendsStackQuickSort extends ArrayList{
+    public ExtendsStackQuickSort() {
+    }
+
+    void quickS() {
+        this.sort(Comparator.naturalOrder());
+//this.
+    }
+
+   
+    
+}
---
+++ src/javaapplication70/Prueba_E_QS.java
@@ -0,0 +1,20 @@
+
+package javaapplication70;
+
+public class Prueba_E_QS {
+
+    public static void main(String[] args) {
+        ExtendsStackQuickSort numeros = new ExtendsStackQuickSort();
+        numeros.add(0X50);
+        numeros.add(0X5);
+        numeros.add(0X450);
+        numeros.add(0X550);
+        numeros.add(0X500);
+        
+        
+        System.out.println(numeros);
+        numeros.quickS();
+        System.out.println(numeros);
+    }
+    
+}
